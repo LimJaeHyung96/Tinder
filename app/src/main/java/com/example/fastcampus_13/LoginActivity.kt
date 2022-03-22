23cfe74e8ad33933e333f7dc8a0686e19f1ce068
LimJaeHyung96 <jhlim96@naver.com>
2022-03-22 15:43:09 +0900

    facebook login commit

--- app/src/main/java/com/example/fastcampus_13/LoginActivity.kt
+++ app/src/main/java/com/example/fastcampus_13/LoginActivity.kt
@@ -1,28 +1,36 @@
 package com.example.fastcampus_13
 
+import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.widget.addTextChangedListener
+import com.facebook.CallbackManager
+import com.facebook.FacebookCallback
+import com.facebook.FacebookException
+import com.facebook.login.LoginResult
+import com.facebook.login.widget.LoginButton
+import com.google.firebase.auth.FacebookAuthProvider
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 
 class LoginActivity : AppCompatActivity() {
 
-    private lateinit var auth : FirebaseAuth
+    private lateinit var auth: FirebaseAuth
+    private lateinit var callbackManager: CallbackManager
 
-    private val emailEditText : EditText by lazy {
+    private val emailEditText: EditText by lazy {
         findViewById(R.id.emailEditText)
     }
 
-    private val passwordEditText : EditText by lazy {
+    private val passwordEditText: EditText by lazy {
         findViewById(R.id.passwordEditText)
     }
 
-    private val loginButton : Button by lazy {
+    private val loginButton: Button by lazy {
         findViewById(R.id.loginButton)
     }
 
@@ -30,15 +38,22 @@ class LoginActivity : AppCompatActivity() {
         findViewById(R.id.signUpButton)
     }
 
+    private val facebookLoginButton: LoginButton by lazy {
+        findViewById(R.id.facebookLoginButton)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
         auth = Firebase.auth //FirebaseAuth.getInstance()랑 같음
+        callbackManager =
+            CallbackManager.Factory.create(); //https://developers.facebook.com/docs/facebook-login/android 참고
 
         initLoginButton()
         initSignUpButton()
         initEmailAndPasswordEditText()
+        initFacebookLoginButton()
     }
 
     private fun initSignUpButton() {
@@ -48,11 +63,12 @@ class LoginActivity : AppCompatActivity() {
 
             auth.signInWithEmailAndPassword(email, password)
                 .addOnCompleteListener(this) {
-                    if(it.isSuccessful){
+                    if (it.isSuccessful) {
                         //메인 위에 띄운 로그인 페이지만 finish 해 줌
                         finish()
                     } else {
-                        Toast.makeText(this, "로그인에 실패했습니다. 이메일 또는 패스워드를 확인해주세요", Toast.LENGTH_SHORT).show()
+                        Toast.makeText(this, "로그인에 실패했습니다. 이메일 또는 패스워드를 확인해주세요", Toast.LENGTH_SHORT)
+                            .show()
                     }
                 }
         }
@@ -65,8 +81,12 @@ class LoginActivity : AppCompatActivity() {
 
             auth.createUserWithEmailAndPassword(email, password)
                 .addOnCompleteListener(this) {
-                    if(it.isSuccessful){
-                        Toast.makeText(this, "회원가입에 성공했습니다. 로그인 버튼을 눌러 로그인 해주세요.", Toast.LENGTH_SHORT).show()
+                    if (it.isSuccessful) {
+                        Toast.makeText(
+                            this,
+                            "회원가입에 성공했습니다. 로그인 버튼을 눌러 로그인 해주세요.",
+                            Toast.LENGTH_SHORT
+                        ).show()
                     } else {
                         Toast.makeText(this, "회원가입에 실패했습니다.", Toast.LENGTH_SHORT).show()
                     }
@@ -88,11 +108,51 @@ class LoginActivity : AppCompatActivity() {
         }
     }
 
-    private fun getInputEmail() : String {
+    private fun initFacebookLoginButton() {
+        facebookLoginButton.setPermissions("email", "public_profile")
+        facebookLoginButton.registerCallback(
+            callbackManager,
+            object : FacebookCallback<LoginResult> {
+                override fun onSuccess(result: LoginResult) {
+                    val crednetial = FacebookAuthProvider.getCredential(result.accessToken.token)
+                    auth.signInWithCredential(crednetial)
+                        .addOnCompleteListener(this@LoginActivity) {
+                            if (it.isSuccessful) {
+                                //메인 위에 띄운 로그인 페이지만 finish 해 줌
+                                finish()
+                            } else {
+                                Toast.makeText(
+                                    this@LoginActivity,
+                                    "페이스북 로그인에 실패했습니다",
+                                    Toast.LENGTH_SHORT
+                                ).show()
+                            }
+                        }
+                }
+
+                override fun onCancel() {
+
+                }
+
+                override fun onError(error: FacebookException?) {
+                    Toast.makeText(this@LoginActivity, "페이스북 로그인에 실패했습니다", Toast.LENGTH_SHORT)
+                        .show()
+                }
+
+            })
+    }
+
+//    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+//        super.onActivityResult(requestCode, resultCode, data)
+//
+//        callbackManager.onActivityResult(requestCode, resultCode, data)
+//    }
+
+    private fun getInputEmail(): String {
         return emailEditText.text.toString()
     }
 
-    private fun getPasswordEmail() : String {
+    private fun getPasswordEmail(): String {
         return passwordEditText.text.toString()
     }
 }
